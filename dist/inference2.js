e2cb53e19032725561c014c27aa7b67563cbb384
Haruto Konno <qingdoujinye@gmail.com>
2021-10-02 00:21:09 +0900

    fix-inference2

---
+++ dist/inference2.js
@@ -0,0 +1,12 @@
+"use strict";
+// function squareOf(n) {
+//   return n * n;
+// }
+// squareOf(5); //25
+// /*squareOf(z);*/ //Nan
+// function sumOf(n: number) {
+//   return n + n;
+// }
+// sumOf(5); //10
+// /*sumOf('z')*/ //エラー
+//# sourceMappingURL=inference2.js.map
