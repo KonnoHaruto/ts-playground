79503bf027a83b88e2f947a824d1f10a69b60888
Haruto Konno <qingdoujinye@gmail.com>
2021-10-01 23:56:42 +0900

    Fire extinguishing

--- src/inference2.ts
+++ src/inference2.ts
@@ -1,13 +1,13 @@
-function squareOf(n) {
-  return n * n;
-}
+// function squareOf(n) {
+//   return n * n;
+// }
 
-squareOf(5); //25
-squareOf(z); //Nan
+// squareOf(5); //25
+// /*squareOf(z);*/ //Nan
 
-function sumOf(n: number) {
-  return n + n;
-}
+// function sumOf(n: number) {
+//   return n + n;
+// }
 
-sumOf(5); //10
-sumOf('z') //エラー
+// sumOf(5); //10
+// /*sumOf('z')*/ //エラー
